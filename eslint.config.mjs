27b4bf2c42548932998405a7437d7574c08a4552
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2025-04-13 22:44:51 +0200

    feat: add es-lint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
